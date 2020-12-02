04e7e511dc280878ca31e0d80c94795f67bd3838
Fredhii <fredhiixd@gmail.com>
2020-12-01 20:17:21 -0500

    add new line at the end

--- 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -14,7 +14,7 @@ namespace _5_print_comb
                 }
                 else
                 {
-                    Console.Write("{0:00}", i);
+                    Console.WriteLine("{0:0}", i);
                 }
             }
         }
