de1d4a4a45f74655afc2e01fad909f761497398f
Fredhii <fredhiixd@gmail.com>
2020-12-02 23:45:00 -0500

    fix?

--- 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -10,7 +10,10 @@ class List
             Console.WriteLine("Index is out of range");
             return myList;
         }
-        myList.Remove(myList[index]);
-        return myList;
+        else
+        {
+            myList.Remove(myList[index]);
+            return myList;
+        }
     }
 }
