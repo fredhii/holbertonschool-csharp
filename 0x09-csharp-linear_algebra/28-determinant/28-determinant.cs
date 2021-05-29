8b5d0f616cd49131820ed3f1ddcbf15e0704b623
fredhii <fredhiixd@gmail.com>
2021-05-28 22:56:26 -0500

    :hammer: Fix determinant

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -7,26 +7,27 @@ class MatrixMath
     ///<summary>Determinant of a matrix.</summary>
     public static double Determinant(double[,] matrix)
     {
-        double[,] bad = new double[,] {{-1}};
-        double det = 0;
-        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
+        double determinantM = 0;
+
+        if (2 == matrix.GetLength(0) && 2 == matrix.GetLength(1))
         {
-            det = (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
+            determinantM = Math.Round((matrix[0, 0]*matrix[1, 1] - matrix[0, 1]*matrix[1, 0]), 2);
         }
-        else if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
+        else if (3 == matrix.GetLength(0) && 3 == matrix.GetLength(1))
         {
-            det = Math.Round(
-                (matrix[0, 0] * matrix[1, 1] * matrix[2, 2]) +
-                (matrix[1, 0] * matrix[2, 1] * matrix[0, 2]) +
-                (matrix[2, 0] * matrix[0, 1] * matrix[1, 2]) -
-                (matrix[0, 0] * matrix[2, 1] * matrix[1, 2]) -
-                (matrix[2, 0] * matrix[1, 1] * matrix[0, 2]) -
-                (matrix[1, 0] * matrix[0, 1] * matrix[2, 2]), 2);
-        }
-        else
-        {
-            return (-1);
-        }
-        return (det);
+            determinantM = Math.Round(
+                (
+                    matrix[0, 0]*matrix[1, 1]*matrix[2, 2] + 
+                    matrix[0, 1]*matrix[1, 2]*matrix[2, 0] +
+                    matrix[0, 2]*matrix[1, 0]*matrix[2, 1]
+                ) -
+                (
+                    matrix[0, 0]*matrix[1, 2]*matrix[2, 1] +
+                    matrix[0, 1]*matrix[1, 0]*matrix[2, 2] +
+                    matrix[0, 2]*matrix[1, 1]*matrix[2, 0]
+                ), 2);
+        } else { return -1; }
+        
+        return determinantM;
     }
 }
